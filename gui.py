a1f358361e4d4a63bf4281c1a16e2b1a0797becd
VU NHU ANH <vunhuanh@gmail.com>
2018-03-09 17:40:40 -0500

    gui3

--- gui.py
+++ gui.py
@@ -6,19 +6,23 @@ from Tkinter import *
 parentw = Tkinter.Tk()
 # Code to add widgets will go here...
 
-
-title = Text(parentw)
-title.insert(INSERT, "GOURMET\n")
+#General intro text
+title = Label(parentw, text="GOURMET\n\n", anchor=CENTER)
 title.pack()
-desc = Text(parentw)
-desc.insert(END, "Gourmet is an application which lets you reserve tables in our partnered restaurants, as well as buy event tickets and food.")
+desc = Label(parentw, text="Gourmet is an application which lets you reserve tables in our partnered restaurants, as well as buy event tickets and food.\n\n", anchor=CENTER)
 desc.pack()
 
+#Login, signup
 L1 = Label(parentw, text="Username")
 L1.pack(side = LEFT)
-E1 = Entry(parentw, bd = 5)
+E1 = Entry(parentw)
 E1.pack(side = RIGHT)
 
+L2 = Label(parentw, text="Password")
+L2.pack(side = LEFT)
+E2 = Entry(parentw)
+E2.pack(side = RIGHT)
 
 
-parentw.mainloop()
+
+mainloop()
