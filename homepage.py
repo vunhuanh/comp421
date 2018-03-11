d7b41caacc3e3787096e23c279560f4b1c4fb2f7
VU NHU ANH <vunhuanh@gmail.com>
2018-03-11 12:19:27 -0400

    GUI revamp with frames

--- homepage.py
+++ homepage.py
@@ -1,54 +1,52 @@
-import Tkinter
-from Tkinter import *
+import Tkinter as tk  
 import psycopg2
 import sqlalchemy
 import pandas.io.sql as psql
 import DBconnection
 from sqlalchemy import Table, Column, String, MetaData
 
+# Frame for home page
+class Homepage(tk.Frame):
 
-class Application(Frame):
-
-    #Customize the master window
-    def __init__(self):
-        self.master = Tk()
-        self.master.geometry("800x500")
-
-        Frame.__init__(self, self.master)
-        self.grid()
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
         self.create_widgets()
 
-    #Create widgets inside of the master window
+    # Show a frame for the given page name
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+    # Create widgets inside homepage
     def create_widgets(self):
-        #Config
+        # Set width to column 0
         self.grid_columnconfigure(0, minsize=100)
-        w1 = Text(self, undo=True, height=1, width=26,wrap=NONE)
-        w1.grid(row=0, column=1, padx=5, pady=5, sticky=W)
         
-        #Header
-        self.title = Label(self, text="GOURMET")
+        # Header
+        self.title = tk.Label(self, text="GOURMET")
         self.title.grid(row=0, column=1)
-        self.desc = Label(self, text="Welcome, <user>.\n\n", wraplength=500)
+        self.desc = tk.Label(self, text="Welcome, <user>.\n\n", wraplength=500)
         self.desc.grid(row=1, column=1)
 
-        #Buttons
-        self.points_btn = Button(self, text="View my points")
+        # Buttons
+        self.points_btn = tk.Button(self, text="View my points")
         self.points_btn.bind('<Button-1>', self.points)
-        self.points_btn.grid(row=2, column=1, sticky=W)
+        self.points_btn.grid(row=2, column=1, sticky=tk.W)
 
-        self.u_resr_btn = Button(self, text="View my upcoming reservations")
+        self.u_resr_btn = tk.Button(self, text="View my upcoming reservations")
         self.u_resr_btn.bind('<Button-1>', self.u_resr)
-        self.u_resr_btn.grid(row=3, column=1, sticky=W)
+        self.u_resr_btn.grid(row=3, column=1, sticky=tk.W)
 
-        self.u_pickup_btn = Button(self, text="View my upcoming pickup orders")
+        self.u_pickup_btn = tk.Button(self, text="View my upcoming pickup orders")
         self.u_pickup_btn.bind('<Button-1>', self.u_pickup)
-        self.u_pickup_btn.grid(row=4, column=1, sticky=W)
+        self.u_pickup_btn.grid(row=4, column=1, sticky=tk.W)
 
-        self.u_event_btn = Button(self, text="View my upcoming events")
+        self.u_event_btn = tk.Button(self, text="View my upcoming events")
         self.u_event_btn.bind('<Button-1>', self.u_event)
-        self.u_event_btn.grid(row=5, column=1, sticky=W)
+        self.u_event_btn.grid(row=5, column=1, sticky=tk.W)
 
-    #View points
+    # View points
     def points(self, event):
         db = DBconnection.connecting()
         conn = db.connect()
@@ -61,22 +59,16 @@ class Application(Frame):
             print(r[0])
             #get the second column
 
-    #View upcoming reservations
+    # View upcoming reservations
     def u_resr(self, event):
-        print ""
+        print "a"
 
-    #View upcoming pickup orders
+    # View upcoming pickup orders
     def u_pickup(self, event):
         print ""
 
-    #View upcoming events
+    # View upcoming events
     def u_event(self, event):
         print ""
 
 
-    #Start the main loop
-    def start(self):
-        self.master.mainloop()
-        
-if __name__=='__main__': 
-    Application().start()
