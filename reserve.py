09e423e84193b6b8156f112f01c3fbeb1897213c
Chris Issa <chris-jorjio.issa@mail.mcgill.ca>
2018-03-18 14:22:41 -0400

    Reservation (90%)

--- reserve.py
+++ reserve.py
@@ -5,6 +5,7 @@ import pandas.io.sql as psql
 import DBconnection
 from sqlalchemy import Table, Column, String, MetaData
 import globalvar
+import datetime
 
 # Frame for buying event tickets
 class Reserve(tk.Frame):
@@ -113,33 +114,87 @@ class MakeReservation(tk.Frame):
         self.quantity.grid(row=4, column=3)
        
     def submitReservation(self,event):
+        #Get user input 
+        self.u_time = self.time2.get()
+        self.u_date = self.date2.get()
+        self.u_quantity = self.quantity.get()
+
         db = DBconnection.connecting()
         conn = db.connect()
         query = "SELECT r_table.licensenb, r_table.tableid, r_table.capacity FROM r_table,(SELECT licensenb, tableid FROM r_table WHERE exists (SELECT licensenb FROM restaurant WHERE LOCALTIME(0) > openinghours AND LOCALTIME(0) < closinghours AND restaurant.licensenb = r_table.licensenb) EXCEPT SELECT reservation_contains.licensenb, reservation_contains.tableid FROM reservation_contains WHERE EXISTS (SELECT licensenb FROM restaurant WHERE LOCALTIME(0) > openinghours AND LOCALTIME(0) < closinghours AND restaurant.licensenb = reservation_contains.licensenb)) as e WHERE r_table.licensenb = e.licensenb AND r_table.tableid = e.tableid AND r_table.licensenb = '{0}';".format(globalvar.lnb_reserve)
         result_set = conn.execute(query)
         conn.close()
 
+        db = DBconnection.connecting()
+        conn = db.connect()
+        query = "SELECT licensenb FROM restaurant WHERE restaurant.licensenb = '{0}' AND restaurant.openinghours < '{1}' AND restaurant.closinghours > '{2}';".format(globalvar.lnb_reserve,self.u_time,self.u_time)
+        result_set2 = conn.execute(query)
+        conn.close()
+
         licenseNB = []
         tables = []
         capty = []
 
-        # for r in result_set:
-        #     licenseNB.append(r[0])
-        #     print(r[0])
-        #     tables.append(r[1])
-        #     print(r[1])
-        #     capty.append(r[2])
-        #     print(r[2])
-        u_time = time2.get()
-        u_date = date2.get()
-        u_quantity = quantity.get()
 
-        print(u_time)
-        print(u_date)
-        print(u_quantity)
+        for r in result_set:
+            licenseNB.append(r[0])
+            print(r[0])
+            tables.append(r[1])
+            print(r[1])
+            capty.append(r[2])
+            print(r[2])
+
+        licenseNB2 = []
+        for r2 in result_set2:
+            licenseNB2.append(r2[0])
+
+        #get total seats available across all tables
+        total_seats = 0
+        for c in capty:
+            total_seats+=c
+
+        print("total seats avail: ")
+        print(total_seats)
+
+        print("total quant input: " + self.u_quantity)
+
+        timestamp = self.u_date + self.u_time
+
+        if(int(self.u_quantity) > total_seats):
+            print("There aren't enough seats to accomodate the number of diners mentioned.")
+            print("Reservation failed")
+
+        #Checking if time input falls between opening hours and closing hours for the restaurant
+        elif(not licenseNB2):
+            print("Nope, not happening.")
+
+        #Verifying date format
+        elif:
+            try:
+                datetime.datetime.strptime(self.u_date, '%Y-%m-%d')
+            except:
+                raise ValueError("Incorrect data format, should be YYY-MM-DD")
+
+        else:
+            db = DBconnection.connecting()
+            conn = db.connect()
+            query = "INSERT INTO reservation VALUES (default, '{0}', '{1}');".format(timestamp,int(self.u_quantity))
+            conn.execute(query)
+            conn.close()
+
+            db = DBconnection.connecting()
+            conn = db.connect()
+            query = "WITH email AS (SELECT cast('{0}' as text) AS var), rid AS (SELECT reservationid AS var FROM reservation WHERE reservationid >= ALL (SELECT reservationid FROM reservation)) INSERT INTO user_books SELECT email.var, rid.var FROM email, rid;".format() #add useremail here (global var)
+            conn.execute(query)
+            conn.close()
+
+            db = DBconnection.connecting()
+            conn = db.connect()
+            query = "WITH att AS (SELECT reservationid AS var FROM (SELECT * FROM reservation WHERE reservationid >= ALL (SELECT reservationid FROM reservation)) as latest WHERE reservationid = latest.reservationid), restau AS (SELECT cast('{0}' as text) AS var), tablenb AS (SELECT 3 AS var) INSERT INTO reservation_contains SELECT att.var, restau.var, tablenb.var FROM att, restau, tablenb;".format(globalvar.lnb_reserve)
+
+
 
         # Go to homepage
     def homepage(self, event):
         self.controller.show_frame("Homepage") 
-
 
