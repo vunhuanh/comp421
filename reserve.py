fb3b04220e1b23c7579eb4fd531f4672cd3a1367
Chris Issa <chris-jorjio.issa@mail.mcgill.ca>
2018-03-24 09:52:08 -0400

    Revert "Merge branch 'chris'"
    This reverts commit cdce534f1a86e4d4d5f0b5266a6528eae19f0f67, reversing
    changes made to 2ee5a42c8236e81534ffa9e21ca238d7327852a1.

--- reserve.py
+++ reserve.py
@@ -5,8 +5,7 @@ import pandas.io.sql as psql
 import DBconnection
 from sqlalchemy import Table, Column, String, MetaData
 from changeglobal import setGlobal, getGlobal
-#import datetime
-import tkMessageBox
+import datetime
 
 # Frame for buying event tickets
 class Reserve(tk.Frame):
@@ -14,29 +13,59 @@ class Reserve(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        self.create_widgets()
-
-    def create_widgets(self):
-        # Set min width to columns
-        self.grid_columnconfigure(0, minsize=150)
-        self.grid_columnconfigure(1, minsize=150)
-        self.grid_columnconfigure(2, minsize=150)
-        self.grid_rowconfigure(2, minsize=10)
-
-        # Display
-        self.display_btn = tk.Button(self, text="Display")
-        self.display_btn.bind('<Button-1>', self.display)
-        self.display_btn.grid(row=1, column=0)
-        
-    # Display page contents
-    def display(self, event):
-        
-        # Header
         self.hp_btn = tk.Button(self, text="Homepage")
         self.hp_btn.bind('<Button-1>', self.homepage)
         self.hp_btn.grid(row=0, column=0)
 
-        self.desc = tk.Label(self, text="Make a reservation", wraplength=400)
+        self.display_btn = tk.Button(self, text="Display")
+        self.display_btn.bind('<Button-1>', self.display)
+        self.display_btn.grid(row=1, column=0)
+
+        #create_widgets(self.interior)
+
+    # def create_widgets(self):
+    #     Set min width to columns
+    #     self.grid_columnconfigure(0, minsize=150)
+    #     self.grid_columnconfigure(1, minsize=150)
+    #     self.grid_columnconfigure(2, minsize=150)
+    #     self.grid_rowconfigure(2, minsize=10)
+
+    #     # Display
+    #     self.display_btn = tk.Button(self, text="Display")
+    #     self.display_btn.bind('<Button-1>', display(self.interior))
+    #     self.display_btn.grid(row=0, column=4)
+
+    def resize(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"), width=985, height=500)
+        
+    # Display page contents
+    def display(self, event):
+
+        self.canvas = tk.Canvas(self, bd=0, highlightthickness=0)
+        self.canvas.grid(row=0, column=0)
+
+        self.vsbar = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.vsbar.grid(row=0, column=1, sticky='ns')
+        self.canvas.configure(yscrollcommand=self.vsbar.set)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = tk.Frame(self.canvas)
+        self.canvas.create_window((0,0), window=self.interior, anchor='nw')
+
+        self.interior.bind("<Configure>", self.resize)
+        self.interior.grid_columnconfigure(0, minsize=150)
+        self.interior.grid_columnconfigure(1, minsize=150)
+        self.interior.grid_columnconfigure(2, minsize=150)
+
+        self.hp_btn = tk.Button(self.interior, text="Homepage")
+        self.hp_btn.bind('<Button-1>', self.homepage)
+        self.hp_btn.grid(row=0, column=0)
+
+        self.display_btn = tk.Button(self.interior, text="Display")
+        self.display_btn.bind('<Button-1>', self.display)
+        self.display_btn.grid(row=1, column=0)
+
+        self.desc = tk.Label(self.interior, text="Make a reservation", wraplength=400)
         self.desc.grid(row=1, column=1)
 
         # Connect to DB and get info
@@ -53,15 +82,15 @@ class Reserve(tk.Frame):
             restau.append(r[1])
 
         # Print relevant info
-        self.name = tk.Label(self, text="Restaurant")
+        self.name = tk.Label(self.interior, text="Restaurant")
         self.name.grid(row=3, column=1)
 
         irow = 4
         i = 0
         for r in restau:
-            self.res = tk.Label(self, text=restau[i])
+            self.res = tk.Label(self.interior, text=restau[i])
             self.res.grid(row=irow, column=1)  
-            self.ures = tk.Button(self, text="Reserve")
+            self.ures = tk.Button(self.interior, text="Reserve")
             print(licensenb[i])
             self.ures.bind('<Button-1>', lambda event, arg=licensenb[i]: self.mkres(event, arg))
             self.ures.grid(row=irow, column=2)
@@ -137,7 +166,6 @@ class MakeReservation(tk.Frame):
 
         licensenb = getGlobal('lnb_reserve')
         useremail = getGlobal('useremail')
-        date = getGlobal('date')
 
         db = DBconnection.connecting()
         conn = db.connect()
@@ -169,28 +197,31 @@ class MakeReservation(tk.Frame):
         for r2 in result_set2:
             licenseNB2.append(r2[0])
 
+        #FOR TESTING PURPOSES, CHECKING IF TABLES SORTED BY TABLE ID NUMBER
+        # print("BREAK///")
+
+        # print(tables)
+        # print(capty)
 
         #get total seats available across all tables
         total_seats = 0
         for c in capty:
             total_seats+=c
 
-        # print("total seats avail: ")
-        # print(total_seats)
+        print("total seats avail: ")
+        print(total_seats)
 
-        # print("total quant input: " + self.u_quantity)
+        print("total quant input: " + self.u_quantity)
 
         timestamp = self.u_date + " " + self.u_time
 
         if(int(self.u_quantity) > total_seats):
-            tkMessageBox.showerror("error","There aren't enough seats to accomodate the number of diners mentioned. Reservation failed.")
+            print("There aren't enough seats to accomodate the number of diners mentioned.")
+            print("Reservation failed")
 
         #Checking if time input falls between opening hours and closing hours for the restaurant
         elif(not licenseNB2):
-            tkMessageBox.showerror("error","Invalid time provided. Please consult restaurant opening and closing hours.")
-
-        elif(self.u_date < date):
-            tkMessageBox.showerror("error","Invalid date provided. Reservation failed.")
+            print("Nope, not happening.")
 
         #Verifying date format
         # elif(validate(self.u_date)):
@@ -247,6 +278,22 @@ class MakeReservation(tk.Frame):
 
                 peopleLeft=peopleLeft-capty[i]
                 i=i+1
+
+
+            # for r in res:
+            #     print(r)
+
+            # db = DBconnection.connecting()
+            # conn = db.connect()
+            # query ="WITH email AS (SELECT '{0}' AS var), rid AS (SELECT reservationid AS var FROM reservation WHERE reservationid >= ALL (SELECT reservationid FROM reservation)) INSERT INTO user_books SELECT email.var, rid.var FROM email, rid;".format(useremail)
+            # conn.execute(query)
+            # conn.close()
+
+            # db = DBconnection.connecting()
+            # conn = db.connect()
+            # query = "WITH att AS (SELECT reservationid AS var FROM (SELECT * FROM reservation WHERE reservationid >= ALL (SELECT reservationid FROM reservation)) as latest WHERE reservationid = latest.reservationid), restau AS (SELECT '{0}' AS var), tablenb AS (SELECT 2 AS var) INSERT INTO reservation_contains SELECT att.var, restau.var, tablenb.var FROM att, restau, tablenb;".format(licensenb)
+            # conn.execute(query)
+            # conn.close()
 
 
         # Go to homepage
